59f8efbafba0b772eab13cb5b8ca04f745472d76
Vitor Nascimento <vitor.gan21@gmail.com>
2016-10-12 17:19:48 -0300

    MedirSelectSort e Respostas
    adciondo função para medir SelectSort e txt com as respostas

--- main.c
+++ main.c
@@ -3,57 +3,82 @@
 #include <time.h>
 #include "ArrayCreator.h"
 #include "Ordenacao.h"
-<<<<<<< HEAD
 #define TAM 5000 //o vetor tem que ser declarado com TAM+1, dentro das funções é chamado com TAM normalmente
-=======
-#define TAM 100000 //o vetor tem que ser declarado com TAM+1, dentro das funções é chamado com TAM normalmente
->>>>>>> origin/master
+
+void MedirSelectSort(FILE *);
 
 int main()
 {
-    int *vetor;
-    int i;
-    clock_t start, end;
-    /*printf("\t\t---SELECTSORT---\n");
-    for (i=1;i<=20;i++){
-        vetor = (int*)malloc((i*TAM+1)*sizeof(int));
-        ordem_crescente(vetor, i*TAM);
-        start = clock();
-        SelectSort(vetor, i*TAM);
-        end = clock();
-        printf("O tempo com %d valores foi -> %f segundos\n", i*TAM, (float)(end-start)/CLOCKS_PER_SEC);
-        free(vetor);
-    }*/
+    FILE *ff;
 
-    printf("\t\t---INSERTSORT---\n");
-    for(i=1;i<=20;i++){
-        vetor = malloc((i*TAM+1)*sizeof(int));
-        ordem_aleatoria(vetor, i*TAM);
-        start = clock();
-        InsertSort(vetor, i*TAM);
-        end = clock();
-        printf("O tempo com %d valores foi -> %.3f segundos\n", i*TAM, (float)(end-start)/CLOCKS_PER_SEC);
-        free(vetor);
-    }
+    ff = fopen("resposta.txt", "w");
+
+    MedirSelectSort(ff);
+
+    fclose(ff);
     return 0;
 
 }
 
-void MedirVetorCrescente(int *vet, int n){
-    ordem_crescente(vet, n);
-    printf("Tempo para Vetor CRESCENTE:\n");
-    printf("\tAlgoritmo de Insercao:\n");
-    printf("\tAlgoritmo de Selecao:\n");
-    printf("\tAlgoritmo de Insercao:\n");
-    printf("\tAlgoritmo de Shell:\n");
-    printf("\tAlgoritmo de Quick:\n");
-    printf("\tAlgoritmo de Heap:\n");
-    printf("\tAlgoritmo de Merge:\n");
-}
+
 
 void ImprimirResultados(clock_t *resultados){
     int i;
     for (i = 1; i <= 20; i++){
         printf("\n(%2d) i = %6d -> %2.2fs", i, i*5000, (float)resultados[i]/CLOCKS_PER_SEC);
+    }
+}
+
+void MedirSelectSort(FILE *ff){
+    int i, j;
+    int *vetor;
+    clock_t start, end, resultados[21];
+
+    fprintf(ff, "--METODO DE SELECAO--\n");
+    printf("--METODO DE SELECAO--\n");
+    fprintf(ff, "\tOrdem Crescente:\n");
+    printf("\tOrdem Crescente:\n");
+    for(i=1; i<=20; i++){
+        vetor = (int*)malloc((i*TAM+1)*sizeof(int));
+        ordem_crescente(vetor, i*TAM);
+        start = clock();
+        SelectSort(vetor, i*TAM);
+        end = clock();
+        resultados[i] = end - start;
+        fprintf(ff, "\t\tO tempo com %6d valores foi -> %.3f segundos\n", i*TAM, (float)resultados[i]/CLOCKS_PER_SEC);
+        printf("\t\tO tempo com %6d valores foi -> %.3f segundos\n", i*TAM, (float)resultados[i]/CLOCKS_PER_SEC);
+        free(vetor);
+    }
+    fprintf(ff, "\n\tOrdem Decrescente:\n");
+    printf("\n\tOrdem Decrescente:\n");
+    for(i=1; i<=20; i++){
+        vetor = (int*)malloc((i*TAM+1)*sizeof(int));
+        ordem_decrescente(vetor, i*TAM);
+        start = clock();
+        SelectSort(vetor, i*TAM);
+        end = clock();
+        resultados[i] = end - start;
+        fprintf(ff, "\t\tO tempo com %6d valores foi -> %2.3f segundos\n", i*TAM, (float)resultados[i]/CLOCKS_PER_SEC);
+        printf("\t\tO tempo com %6d valores foi -> %.3f segundos\n", i*TAM, (float)resultados[i]/CLOCKS_PER_SEC);
+        free(vetor);
+    }
+
+    for(i=1; i<=20;i++)
+        resultados[i] = 0;
+
+    fprintf(ff, "\n\tOrdem Aleatoria:\n");
+    printf("\n\tOrdem Aleatoria:\n");
+    for(i=1; i<=20; i++){
+        vetor = (int*)malloc((i*TAM+1)*sizeof(int));
+        for(j=1; j<=10;j++){
+            ordem_aleatoria(vetor, i*TAM);
+            start = clock();
+            SelectSort(vetor, i*TAM);
+            end = clock();
+            resultados[i] += end - start;
+        }
+        fprintf(ff, "\t\tO tempo com %6d valores foi -> %.3f segundos\n", i*TAM, (float)resultados[i]/CLOCKS_PER_SEC/10);
+        printf("\t\tO tempo com %6d valores foi -> %.3f segundos\n", i*TAM, (float)resultados[i]/CLOCKS_PER_SEC/10);
+        free(vetor);
     }
 }
